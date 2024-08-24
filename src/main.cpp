f8be17193dc37f31c9a3953cfd2be6b2f9592642
Terungwa_Ayerhan <terayerhan@outlook.com>
2024-08-24 11:08:03 +1000

    Take coordinate inputs from user in main.cpp
    Used the recieved coordinates from the user to initialize the
    rout_planner object.

--- src/main.cpp
+++ src/main.cpp
@@ -55,12 +55,22 @@ int main(int argc, const char **argv)
     // TODO 1: Declare floats `start_x`, `start_y`, `end_x`, and `end_y` and get
     // user input for these values using std::cin. Pass the user input to the
     // RoutePlanner object below in place of 10, 10, 90, 90.
+    // Declare four variables to store the start and end coordinates.
+    float start_x, start_y, end_x, end_y;
+
+    // Prompt the user for input
+    std::cout << "Enter start and end coordinates in the form: start_x start_y end_x end_y "<<std::endl;
+
+    // Take inputs form the user
+    std::cin >> start_x >>start_y >> end_x >> end_y;
+    // Display what was received
+    std::cout << "start_x = " << start_x << ", start_y = " << start_y << ", end_x = " << end_x << ", end_y = " << end_y <<std::endl;
 
     // Build Model.
     RouteModel model{osm_data};
 
     // Create RoutePlanner object and perform A* search.
-    RoutePlanner route_planner{model, 10, 10, 90, 90};
+    RoutePlanner route_planner{model, start_x, start_y, end_x, end_y};
     route_planner.AStarSearch();
 
     std::cout << "Distance: " << route_planner.GetDistance() << " meters. \n";
